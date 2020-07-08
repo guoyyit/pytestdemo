7c43e06545379951f12bde663d22733f8d03949f
guoyangyang <guoyangyang@sinoiov.com>
2020-07-08 15:32:48 +0800

    更新代码。。。

---
+++ datastructure/test.py
@@ -0,0 +1,16 @@
+#python list集合 使用方式
+list = [5,7,1,8,0,2,7,9,3,6]
+print(list[0])
+
+#python 数组 使用方式
+array = (1,2,3,4,5,6)
+print(array[1:5])
+
+#python set集合 使用方式
+set = {'Google', 'Taobao', 'Runoob', 'Facebook', 'Zhihu', 'Baidu','Taobao'}
+print(set)
+
+#python map 使用方式
+tinydict = {'name': 'runoob','code':1, 'site': 'www.runoob.com'}
+print(tinydict.keys())
+print(tinydict.values())
